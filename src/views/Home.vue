a91e4ac02d5d30dd73e1720b5f39871b1541e96d
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-11-07 20:13:56 +0100

    [WiP]: Work in Progress

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1 class="header">Home Page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  color: $c-red;
+}
+</style>
