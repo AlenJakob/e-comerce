4936ad0f3271845e7efaee4a84c5fa6959490d28
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-11-26 21:28:59 +0100

    #2-router: clean up route file

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,126 @@
+<template>
+  <main class="product-details">
+    <div class="product-header">
+      <h3 class="product-header__brand">{{ itemInfo.brand_name }}</h3>
+      <h1 class="product-header__heading">{{ itemInfo.name }}</h1>
+      <article class="product-header__content">
+        {{ itemInfo.description }}
+      </article>
+    </div>
+
+    <div class="product-summary">
+      <p class="product-summary__price">${{ itemInfo.price }}.00</p>
+      <p class="product-summary__discount">${{ itemInfo.price / 2 }}0</p>
+    </div>
+
+    <div class="product-details">
+      <button
+        class="product-details__btn-size"
+        :class="{ 'product-details__btn-size--active': index === 2 }"
+        v-for="(size, index) in SHOE_CALCULATE(itemInfo.size_range)"
+        :key="index"
+      >
+        {{ size }}
+      </button>
+      <p class="product-details__color">
+        color:
+        <span class="product-details__color--label">{{ itemInfo.color }}</span>
+      </p>
+    </div>
+
+    <div class="product-cta">
+      <button class="product-cta__button">-</button>
+      <span class="product-cta__value">0</span>
+      <button class="product-cta__button">+</button>
+      <product-button text="Add to cart"></product-button>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+// NOTE: method accept array
+import SHOE_CALCULATE from "@/helpers/index";
+import ProductButton from "@/components/ui/utils/ProductButton";
+
+defineProps({
+  itemInfo: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
+<style lang="scss" scoped>
+.product-details {
+  flex: 1;
+}
+.product-header {
+  &__brand {
+    color: $c-orange;
+    font-weight: $fw-bold;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    margin-bottom: 1rem;
+    font-size: 14px;
+  }
+  &__heading {
+    margin-bottom: 2rem;
+    color: $c-very-dark-blue;
+  }
+  &__content {
+    color: $c-dark-grayish-blue;
+    margin-bottom: 2rem;
+    line-height: 1.5rem;
+  }
+}
+.product-summary {
+  margin-bottom: 2rem;
+
+  &__price {
+    font-size: 2rem;
+    font-weight: $fw-bold;
+    color: $c-very-dark-blue;
+    letter-spacing: 1px;
+    margin-bottom: 0.5rem;
+  }
+  &__discount {
+    font-size: 1rem;
+    font-weight: $fw-bold;
+    color: $c-grayish-blue;
+    letter-spacing: 1px;
+    text-decoration: line-through;
+  }
+}
+// .product-details__btn-size--active // active state of marked shoe size
+// TODO: method that allows to choose only 1 size of shoes
+.product-details {
+  margin: 1rem 0;
+  &__btn-size {
+    background: #f1f1f1;
+    border: 2px dashed transparent;
+    border-radius: 4px;
+    margin: 0 0.4rem;
+    color: $c-dark-gray;
+    font-weight: $fw-bold;
+    height: 40px;
+    width: 40px;
+    &:first-child {
+      margin-left: 0;
+    }
+    &--active {
+      border-color: rgba($c-dark-gray, 0.6);
+    }
+  }
+  &__color {
+    margin: 2rem 0;
+  }
+  &__color--label {
+    margin-left: 8px;
+    font-weight: $fw-bold;
+    border: 2px dashed rgba($c-dark-gray, 0.4);
+    border-radius: 4px;
+    font-size: 1.2rem;
+    padding: 2px 12px;
+  }
+}
+</style>
---
+++ src/components/ui/utils/ProductButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="product-button">
+    <slot></slot>
+    <span>{{ props.text }}</span>
+  </button>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  text: {
+    type: String,
+    default: "click",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.product-button {
+  margin: 2rem 0;
+}
+</style>
