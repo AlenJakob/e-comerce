2caa130b54103960cbb9c313bb0f7acb45af9501
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-12-05 18:34:41 +0100

    #7product-gallery: add glide js gallery for products pormotion list

---
+++ src/components/ui/ProductsSlider.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="glide">
+    <div class="glide__track" data-glide-el="track">
+      <ul class="glide__slides">
+        <li v-for="n in 5" :key="n">
+          <img
+            class="glide__slide-img"
+            alt="tmp alt please put something here"
+            src="https://img.eobuwie.cloud/card_desktop_430w(wysiwyg/eobuwie/akcjeGlobalny/12_2021/newsg/fila_874X874.jpg)/fila_874X874.jpg"
+          />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import Glide from "@glidejs/glide/dist/glide.esm";
+
+onMounted(async () => {
+  const glide = await new Glide(".glide", {
+    gap: 20,
+    perView: 4,
+    startAt: 0,
+    focusAt: "center",
+    type: "carousel",
+    autoplay: 1,
+    animationDuration: 3000,
+  });
+  glide.mount();
+});
+</script>
+<style lang="scss" scoped>
+.glide {
+  overflow: hidden;
+}
+.glide__slides {
+  display: flex;
+  max-width: 300px;
+}
+.glide__slide-img {
+  max-width: 300px;
+}
+</style>
