794a135e4b74564e6c3570c95293cd7125a2da70
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-12-17 17:52:46 +0100

    create mock & add to men & women site

---
+++ src/services/Women/index.js
@@ -0,0 +1,15 @@
+// import axios from "axios";
+
+// async function fetchCollection() {
+//   return await axios
+//     .get("https://github.com/Stupidism/goat-sneakers/blob/master/api.json")
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+import SHOES_COLLECTION_WOMEN from "./SHOES_COLLECTION_WOMEN";
+
+export default SHOES_COLLECTION_WOMEN;
