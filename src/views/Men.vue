039b45f18ce39b43f10aa88941e9d5d0538b61fe
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-12-17 18:37:28 +0100

    #11 add mock data

--- src/views/Men.vue
+++ src/views/Men.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <product-box v-for="item in state.productList.shoes" :key="item">
+    <product-box v-for="item in menShoes" :key="item">
       <template v-slot:gallery>
         <product-gallery
           :images="item.main_image"
@@ -19,7 +19,11 @@ import ProductBox from "@/components/TheBox/ProductBox.vue";
 import ProductGallery from "@/components/ProductGallery";
 import ProductDetails from "@/components/ProductDetails";
 import SHOES_COLLECTION from "@/services/collections/index";
-import { reactive } from "vue";
+import { reactive, computed } from "vue";
 
 const state = reactive({ productList: SHOES_COLLECTION });
+
+const menShoes = computed(() => {
+  return state.productList.shoes.filter((item) => item.gender.includes("men"));
+});
 </script>
