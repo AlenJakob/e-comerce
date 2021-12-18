cae86bbb78d34b548512ac11716ecf92eb2eee5e
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-12-18 17:25:25 +0100

    #11 reverse list to show

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <product-box v-for="item in productListClone" :key="item">
+    <product-box v-for="item in result" :key="item">
       <template v-slot:gallery>
         <product-gallery
           :images="item.main_image"
@@ -19,13 +19,26 @@ import ProductBox from "@/components/TheBox/ProductBox.vue";
 import ProductGallery from "@/components/ProductGallery";
 import ProductDetails from "@/components/ProductDetails";
 import SHOES_COLLECTION from "@/services/collections/index";
-import { onMounted, reactive } from "vue";
+import { defineProps, reactive, computed } from "vue";
 
-const state = reactive({ productList: SHOES_COLLECTION });
-const productListClone = JSON.parse(
-  JSON.stringify(state.productList.shoes)
-).reverse();
-onMounted(() => {
-  console.log();
+const props = defineProps({
+  id: {
+    type: [String, Number],
+  },
 });
+const state = reactive({ productList: SHOES_COLLECTION });
+
+// const productListClone = JSON.parse(JSON.stringify(state.productList.shoes));
+
+const result = computed(() => {
+  return state.productList.shoes.filter(
+    (item) => Number(item.id) === Number(props.id)
+  );
+});
+
+// Temporary get item by given id
+
+// const shoeItem = () => {
+//   return;
+// };
 </script>
