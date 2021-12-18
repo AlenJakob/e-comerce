14de40aad25bbb06ee5daafc9d6f1bfdd25bbbc9
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-12-18 20:26:36 +0100

    #12 instead of loader used transaition

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,10 @@
         ></product-full-screen-gallery>
       </transition>
     </div>
-    <router-view />
+    <router-view v-slot="{ Component }"
+      ><transition name="route" mode="out-in">
+        <component :is="Component" /></transition
+    ></router-view>
   </div>
 </template>
 
@@ -42,5 +45,23 @@ body {
 html {
   overflow: -moz-scrollbars-vertical;
   overflow-y: scroll;
+}
+</style>
+
+<style lang="scss" scoped>
+/* route transitions */
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
